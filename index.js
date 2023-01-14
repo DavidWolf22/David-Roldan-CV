81103c8ed7258b0567fdff4716920ec7fb7f6ea1
David Roldan <droldan1990@gmail.com>
2023-01-14 20:00:33 -0300

    Primera base de la web CV

---
+++ index.js
@@ -0,0 +1,38 @@
+//login
+function login()
+	{
+
+	let user=document.getElementById("usuario").value;
+	let pass=document.getElementById("clave").value;
+	
+ 
+	if(user=="vista" && pass=="vista")
+	{
+		window.location="index.html";
+	}
+	else{
+		alert("Datos Incorrectos");
+	}
+
+	}
+
+//boton ir arriba
+
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+    $(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+
+});
